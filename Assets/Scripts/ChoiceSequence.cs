d738bd12f70ac5c7bda6b9aaecd6b9002a83f695
PawnCode <nathpros@hotmail.fr>
2019-02-27 12:30:49 +0100

    Resolve merge conflict

--- Assets/Scripts/ChoiceSequence.cs
+++ Assets/Scripts/ChoiceSequence.cs
@@ -7,10 +7,13 @@ public class ChoiceSequence : MonoBehaviour
     public List<Sequence> sequences;
     public bool nextSequence = false;
     public bool blockHide = false;
+    public bool epilogue = false;
+
+    private float _fadePercent = 1;
 
     private void Start()
     {
-        if(sequences.Count == 0)
+        if(epilogue)
         {
             if(!DirectorSequencer.Instance.showEpilogue)
             {
@@ -27,5 +30,45 @@ public class ChoiceSequence : MonoBehaviour
     public List<Sequence> GetSequence()
     {
         return sequences;
+    }
+
+    public void FadeOutSequence()
+    {
+        StopAllCoroutines();
+        StartCoroutine(CO_FadeOutChoiceSequence());
+    }
+
+    public void FadeInSequence()
+    {
+        StopAllCoroutines();
+        StartCoroutine(CO_FadeInChoiceSequence());
+    }
+
+    IEnumerator CO_FadeOutChoiceSequence()
+    {
+        while (_fadePercent > 0)
+        {
+            _fadePercent -= Time.deltaTime / 3;
+            GetComponent<AudioSource>().volume = _fadePercent;
+            yield return null;
+        }
+        _fadePercent = 0;
+        GetComponent<AudioSource>().volume = 0;
+        DirectorSequencer.Instance.ValidateChoice(this);
+
+    }
+
+    IEnumerator CO_FadeInChoiceSequence()
+    {
+        while (_fadePercent < 1)
+        {
+            _fadePercent += Time.deltaTime;
+            GetComponent<AudioSource>().volume = _fadePercent;
+            yield return null;
+        }
+
+        _fadePercent = 1;
+        GetComponent<AudioSource>().volume = 1;
+
     }
 }
